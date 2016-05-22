095f91cc86e0f58d43c530ca234d90f1c8571ed6
Novik <bogdan.novosad.novik@gmail.com>
2016-05-22 23:11:18 +0300

    Change connection string

--- Rozklad/Program.cs
+++ Rozklad/Program.cs
@@ -6,55 +6,140 @@ namespace Rozklad
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static readonly string connectionString =
+            "Data Source=SQL5026.Smarterasp.net;Initial Catalog=DB_9FEAE4_rozklad;User Id=DB_9FEAE4_rozklad_admin;Password=q1234567";
+
+        public static void GetMondaySchedule()
         {
-            var connectionString =
-                "Data Source=DESKTOP-ECVPNVB;Initial Catalog=Rozklad;Integrated Security=True";
-
-            using (var connection =
-                new SqlConnection(connectionString))
+            try
             {
-                try
-                {
-                    Console.WriteLine(connection.ConnectionTimeout);
-                    Console.WriteLine(connection.Database);
-                    Console.WriteLine(connection.ClientConnectionId);
-                    Console.WriteLine(connection.DataSource);
+                var connection = new SqlConnection(connectionString);
 
-                    var cmd = new SqlCommand();
-                    cmd.CommandText = "SELECT " +
-                                      "[pName] ," +
-                                      "[pParaN] ," +
-                                      "[pDayOfWeek] ," +
-                                      "[pCH] " +
-                                      "FROM tParu where pDayOfWeek = 2 order by pParaN";
-                    cmd.CommandType = CommandType.Text;
-                    cmd.Connection = connection;
-                    connection.Open();
-                    var myDataReader = cmd.ExecuteReader();
-                    while (myDataReader.Read())
+                var cmd = new SqlCommand();
+                cmd.CommandText = "SELECT " +
+                                  "[pName] ," +
+                                  "[pParaN] ," +
+                                  "[pDayOfWeek] ," +
+                                  "[pCH] " +
+                                  "FROM tParu where pDayOfWeek = 1 order by pParaN";
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = connection;
+                connection.Open();
+                var myDataReader = cmd.ExecuteReader();
+                while (myDataReader.Read())
+                {
+                    int N;
+                    string Name;
+                    var Ch = true;
+                    int dayOfWeek;
+                    Name = myDataReader["pName"].ToString();
+                    dayOfWeek = int.Parse(myDataReader["pDayOfWeek"].ToString());
+                    N = int.Parse(myDataReader["pParaN"].ToString());
+                    //Ch = bool.Parse(myDataReader["pCH"].ToString());
+                    Console.Write(N + " - " + Name + " - ");
+                    switch (dayOfWeek)
                     {
-                        int N;
-                        string Name;
-                        bool Ch = true;
-                        int dayOfWeek;
-                        Name = myDataReader["pName"].ToString();
-                        dayOfWeek = int.Parse(myDataReader["pDayOfWeek"].ToString());
-                        N = int.Parse(myDataReader["pParaN"].ToString());
-                        //Ch = bool.Parse(myDataReader["pCH"].ToString());
-                        Console.WriteLine($"{N} - {Name} - {dayOfWeek} - {Ch}");
+                        case 1:
+                            Console.WriteLine(DayOfWeek.Monday);
+                            break;
+                        case 2:
+                            Console.WriteLine(DayOfWeek.Thursday);
+                            break;
+                        case 3:
+                            Console.WriteLine(DayOfWeek.Wednesday);
+                            break;
+                        case 4:
+                            Console.WriteLine(DayOfWeek.Thursday);
+                            break;
+                        case 5:
+                            Console.WriteLine(DayOfWeek.Friday);
+                            break;
+                        case 6:
+                            Console.WriteLine(DayOfWeek.Saturday);
+                            break;
+                        case 7:
+                            Console.WriteLine(DayOfWeek.Sunday);
+                            break;
                     }
                 }
+            }
+            catch (SqlException sqlException)
+            {
+                //Console.WriteLine(sqlException);
+            }
+        }
 
-                catch (SqlException sqlException)
+        public static void GetAllSchedule()
+        {
+            try
+            {
+                var connection = new SqlConnection(connectionString);
+
+
+                var cmd = new SqlCommand();
+                cmd.CommandText = "SELECT " +
+                                  "[pName] ," +
+                                  "[pParaN] ," +
+                                  "[pDayOfWeek] ," +
+                                  "[pCH] " +
+                                  "FROM tParu order by pDayOfWeek,pParaN";
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = connection;
+                connection.Open();
+                var myDataReader = cmd.ExecuteReader();
+                while (myDataReader.Read())
                 {
-                    //Console.WriteLine(sqlException);
-                }
-                finally
-                {
-                    connection.Close();
+                    int N;
+                    string Name;
+                    var Ch = true;
+                    int dayOfWeek;
+                    Name = myDataReader["pName"].ToString();
+                    dayOfWeek = int.Parse(myDataReader["pDayOfWeek"].ToString());
+                    N = int.Parse(myDataReader["pParaN"].ToString());
+                    //Ch = bool.Parse(myDataReader["pCH"].ToString());
+                    Console.Write(N + " - " + Name + " - ");
+                    switch (dayOfWeek)
+                    {
+                        case 1:
+                            Console.WriteLine(DayOfWeek.Monday);
+                            break;
+                        case 2:
+                            Console.WriteLine(DayOfWeek.Tuesday);
+                            break;
+                        case 3:
+                            Console.WriteLine(DayOfWeek.Wednesday);
+                            break;
+                        case 4:
+                            Console.WriteLine(DayOfWeek.Thursday);
+                            break;
+                        case 5:
+                            Console.WriteLine(DayOfWeek.Friday);
+                            break;
+                        case 6:
+                            Console.WriteLine(DayOfWeek.Saturday);
+                            break;
+                        case 7:
+                            Console.WriteLine(DayOfWeek.Sunday);
+                            break;
+                    }
                 }
             }
+            catch (SqlException sqlException)
+            {
+                //Console.WriteLine(sqlException);
+            }
+        }
+
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("All schedule");
+            GetAllSchedule();
+            Console.WriteLine();
+            Console.WriteLine("Monday schedule");
+            GetMondaySchedule();
+            Console.WriteLine();
+            Console.ReadKey();
         }
     }
 }
